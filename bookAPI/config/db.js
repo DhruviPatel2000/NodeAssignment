c1abc84c2fff7fd6ad2575ac413a50fe75cdd898
DhruviPatel2000 <81283088+DhruviPatel2000@users.noreply.github.com>
2022-02-16 16:53:04 +0530

    Delete bookAPI directory

--- bookAPI/config/db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DBURL ,{ useNewUrlParser: true } );
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-module.exports=mongoose;
